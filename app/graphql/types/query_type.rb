56987f1fa4a2f4ef41cefbd65d19a2d181a7088b
Cassio Santana <cassioo.roger@gmail.com>
2023-06-12 11:59:54 -0300

    feat: add game query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,12 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :game, Types::Game::GameType, null: false, description: "Fetch a game by ID" do
+      argument :id, ID, required: true
+    end
+
+    def game(id:)
+      ::Game.find(id)
     end
   end
 end
