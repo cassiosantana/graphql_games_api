e9e1dc2ba730e14ce8f5946a92aa834b026e5ae4
Cassio Santana <cassioo.roger@gmail.com>
2023-06-12 10:47:38 -0300

    build: add associations between game and character

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,4 @@
 class Character < ApplicationRecord
+  has_many :projects
+  has_many :games, through: :projects
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+  has_many :projects
+  has_many :characters, through: :projects
 end
